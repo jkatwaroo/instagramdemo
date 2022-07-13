032b42009d765396d6de8f6c9acae5a70186ebad
Jason Katwaroo <jasonkatwaroo@gmail.com>
2022-07-13 07:10:49 -0400

    Initial commit of InstagramDemo Project

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row">
+    <div class="col-3 p-5">
+        <img src="https://pbs.twimg.com/profile_images/1276770212927410176/qTgTIejk_400x400.jpg" style="height:150px;"class="rounded-circle">
+    </div>
+    <div class="col-9 pt-5">
+        <div class="d-flex justify-content-between align-items-baseline">
+            <h1>{{$user->username}}</h1>
+            <a href="/p/create">Add New Post</a>
+        </div>
+        <div class="d-flex">
+            <div class="pe-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+            <div class="pe-5"><strong>23k</strong> followers</div>
+            <div class="pe-5"> <strong>212</strong> following</div>
+        </div>
+        <div class="pt-4 fw-bold">{{ $user->profile->title }}</div>
+        <div>{{ $user->profile->description }}</div>
+        <div><a href="#">{{ $user->profile->url}}</a></div>
+    </div>
+</div>
+<div class="row pt-5">
+    @foreach($user->posts as $post)
+    <div class="col-4 pb-4">
+        <a href="/p/{{  $post->id }}">
+            <img src="/storage/{{$post->image}}" class="w-100">
+        </a>
+    </div>
+    @endforeach
+
+</div>
+
+</div>
+@endsection
