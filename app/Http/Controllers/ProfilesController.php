e83086ee361a6076dacf72e07f09dcd7be4e2e1f
Jason Katwaroo <jasonkatwaroo@gmail.com>
2022-07-13 09:20:28 -0400

    Edit Profile functionality

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,14 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
-    public function index($user)
+    public function index(\App\Models\User $user)
     {
-        $user= User::findOrFail($user);
+       
+        //$user= User::findOrFail($user);
+
         return view('profiles.index',[
             'user' => $user
         ]);
+    }
+
+    public function edit(\App\Models\User $user)
+    {
+        $this->authorize('update',$user->profile);
+        return view('profiles.edit',[
+            'user' => $user
+        ]);
+    }
+
+    public function update(\App\Models\User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => ''
+
+        ]);
+
+
+        if(request('image'))
+        {
+            $imagePath = request('image')->store('profile','public');
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+            $image->save();
+        }
+
+        auth()->user()->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath]
+        ));
+
+        return redirect("/profile/{$user->id}");
     }
 }
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,13 +4,20 @@
 <div class="container">
 <div class="row">
     <div class="col-3 p-5">
-        <img src="https://pbs.twimg.com/profile_images/1276770212927410176/qTgTIejk_400x400.jpg" style="height:150px;"class="rounded-circle">
+        <img src="/storage/{{ $user->profile->image }}" style="height:150px;"class="rounded-circle w-100" >
     </div>
     <div class="col-9 pt-5">
         <div class="d-flex justify-content-between align-items-baseline">
             <h1>{{$user->username}}</h1>
+
+        @can('update',$user->profile)
             <a href="/p/create">Add New Post</a>
+        @endcan
+           
         </div>
+        @can('update',$user->profile)
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+        @endcan
         <div class="d-flex">
             <div class="pe-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
             <div class="pe-5"><strong>23k</strong> followers</div>
